b9639f33f604746473e805a6a073fffb1768590c
Niki Brown <nikibrown@Niki-Browns-MacBook-Pro.local>
2021-01-25 09:12:35 -0800

    sections setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,53 @@
-const { watch, series } = require("gulp");
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const browserSync = require("browser-sync").create();
-const fileinclude = require("gulp-file-include");
+const { watch, series } = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const fileinclude = require('gulp-file-include');
+const sassGlob = require('gulp-sass-glob');
 
 browserSync.init({
-	server: {
-		baseDir: "./docs",
-		index: "/index.html",
-	},
+    server: {
+        baseDir: './docs',
+        index: '/index.html'
+    }
 });
 
 function style() {
-	return gulp
-		.src("./src/assets/scss/*.scss")
-		.pipe(sass())
-		.on("error", sass.logError)
-		.pipe(gulp.dest("./docs/assets/css"));
+    return gulp
+        .src('./src/assets/scss/global.scss')
+        .pipe(sassGlob())
+        .pipe(sass())
+        .on('error', sass.logError)
+        .pipe(gulp.dest('./docs/assets/css'));
 }
 
 function moveimg() {
-	return gulp
-		.src(["./src/assets/img/*"])
-		.pipe(gulp.dest("./docs/assets/img"));
+    return gulp.src(['./src/assets/img/*']).pipe(gulp.dest('./docs/assets/img'));
 }
 
 function movejs() {
-	return gulp
-		.src(["./src/assets/js/*"])
-		.pipe(gulp.dest("./docs/assets/js"));
+    return gulp.src(['./src/assets/js/*']).pipe(gulp.dest('./docs/assets/js'));
+}
+
+function catMoveJS() {
+    return gulp.src('./src/**/*.html').pipe(useref()).pipe(gulp.dest('docs'));
 }
 
 function include() {
-	return gulp
-		.src(["./src/*.html"])
-		.pipe(
-			fileinclude({
-				prefix: "@@",
-				basepath: "@file",
-			})
-		)
-		.pipe(gulp.dest("./docs"));
+    return gulp
+        .src(['./src/*.html'])
+        .pipe(
+            fileinclude({
+                prefix: '@@',
+                basepath: '@file'
+            })
+        )
+        .pipe(gulp.dest('./docs'));
 }
 
 exports.default = function () {
-	gulp.watch("./src/**/*").on(
-		"change",
-		series(style, include, moveimg, movejs, browserSync.reload)
-	);
+    gulp.watch('./src/**/*').on(
+        'change',
+        series(style, include, moveimg, movejs, browserSync.reload)
+    );
 };
